22ef748f2ee49b5fcc85d9b9ceeff6ac4836992f
SweetyRamy <160237649+SweetyRamy@users.noreply.github.com>
2024-02-16 19:20:38 +0530

    new file is added

---
+++ sample2.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello matilda cloud");
+                System.out.println("Hello matilda1 cloud");
+                System.out.println("Hello matilda2 cloud");
+
+
+	}
+
+}
